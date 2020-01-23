e6435b0b5a7e3b73811ed07e057cc04a2b7c31fc
jlsanchezdelgado95 <kokekui17@gmail.com>
2020-01-23 02:06:21 +0100

    Alertas pantalla login terminados

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -16,34 +16,67 @@ export class LoginComponent implements OnInit {
 
   email = new FormControl('');
   password = new FormControl('');
+  closedRegis1 = true;
+  closedRegis2 = true;
+  closedRegis3 = true;
 
-  constructor(private firebase: AngularFirestore, private authService: AuthenticationService
-    , private router: Router) {
+  closedLogin1 = true;
+  closedLogin2 = true;
+  closedLogin3 = true;
+
+  constructor(private firebase: AngularFirestore, private authService: AuthenticationService,
+    private router: Router) {
   }
 
   ngOnInit() {
   }
 
   acceder() {
+    this.resetAlertsLogin();
     this.authService.accederService(this.email.value, this.password.value).then(res => {
       //console.log("Logueado");
       this.router.navigateByUrl("/main");
     })
       .catch(e => {
         console.log("Problemas con el acceso: ", e.message)
+        if (e.message == "There is no user record corresponding to this identifier. The user may have been deleted") {
+          this.closedLogin1 = false;
+        } else if (e.message == "The password is invalid or the user does not have a password.") {
+          this.closedLogin2 = false;
+        } else if (e.message == "The email address is badly formatted.") {
+          this.closedLogin3 = false;
+        }
       });
   }
 
+  resetAlertsLogin() {
+    this.closedLogin1 = true;
+    this.closedLogin2 = true;
+    this.closedLogin3 = true;
+  }
+
   registrarse() {
+    this.resetAlertsRegis();
     this.authService.registarseService(this.email.value, this.password.value).then(res => {
       console.log("Registrado");
     })
       .catch(e => {
         console.log("No se ha podido registrar", e.message)
         if (e.message == "The email address is already in use by another account.") {
-          console.log("Se esta usando este correo electrónico");
+          this.closedRegis2 = false;
+          //console.log("Se esta usando este correo electrónico");
+        } else if (e.message == "The email address is badly formatted.") {
+          this.closedRegis1 = false;
+        } else if (e.message == "The password must be 6 characters long or more.") {
+          this.closedRegis3 = false;
         }
       });
+  }
+
+  resetAlertsRegis() {
+    this.closedRegis1 = true;
+    this.closedRegis2 = true;
+    this.closedRegis3 = true;
   }
 
 }
